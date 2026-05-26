be6666d12f05d7097edee65bc1a48fd2cae7553b
unknown <xioaruan@gmail.com>
2026-05-26 10:30:38 +0800

    Auto-update: 2026/05/26 周二 10:30:33.71

---
+++ 正在学/现代密码学/思维导图/对称加密.mm
@@ -0,0 +1,120 @@
+<map version="freeplane 1.12.15">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<bookmarks/>
+<node TEXT="对称加密" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1779762049504" CHILD_NODES_LAYOUT="TOPTOBOTTOM_RIGHT_AUTO">
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<hook NAME="MapStyle" background="#eeeeeeff">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" auto_compact_layout="true" show_note_icons="true" fit_to_viewport="false" show_icons="BESIDE_NODES" associatedTemplateLocation="template:/light_grey_straight_template.mm" show_tags="UNDER_NODES" showTagCategories="false"/>
+    <tags category_separator="::"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_1082331723" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#222222" BACKGROUND_COLOR="#eeeeee" STYLE="rectangle" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" VGAP_QUANTITY="3 pt">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1082331723" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="horizontal" COLOR="#444444" WIDTH="3" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.tags">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#f6f9a1" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#ffffff" BACKGROUND_COLOR="#3333ff" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#3333ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_334768726" COLOR="#443333" BACKGROUND_COLOR="#dddddd" BORDER_WIDTH="3 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#cc0033">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#cc0033" TRANSPARENCY="255" DESTINATION="ID_334768726"/>
+<font SIZE="12" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#000000" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#2c2b29" BORDER_DASH_LIKE_EDGE="true">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#ffffff" BACKGROUND_COLOR="#222222" BORDER_COLOR="#2c2b29">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#ffffff" BACKGROUND_COLOR="#444444" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#ffffff" BACKGROUND_COLOR="#676767" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#ffffff" BACKGROUND_COLOR="#888888" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#222222" BACKGROUND_COLOR="#aaaaaa" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" COLOR="#222222" BACKGROUND_COLOR="#bbbbbb" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" COLOR="#222222" BACKGROUND_COLOR="#cccccc" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" COLOR="#222222" BACKGROUND_COLOR="#dddddd" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" COLOR="#222222" BACKGROUND_COLOR="#eeeeee" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" COLOR="#2c2b29" BACKGROUND_COLOR="#ffffff" BORDER_COLOR="#f0f0f0">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="信息理论安全/完美安全" FOLDED="true" POSITION="bottom_or_right" ID="ID_1638758784" CREATED="1779761994129" MODIFIED="1779762023294">
+<node TEXT="无限的计算能力" ID="ID_127072933" CREATED="1779762077234" MODIFIED="1779762080771"/>
+</node>
+<node TEXT="计算安全" POSITION="top_or_left" ID="ID_784929834" CREATED="1779762040086" MODIFIED="1779762053459">
+<node TEXT="有效 的敌手" ID="ID_1195328269" CREATED="1779762059066" MODIFIED="1779762064996"/>
+<node TEXT="潜在的成功概率" ID="ID_183299842" CREATED="1779762065280" MODIFIED="1779762069681"/>
+<node TEXT="（t,Ω)实验" ID="ID_626951079" CREATED="1779762092290" MODIFIED="1779762107244"/>
+<node TEXT="渐进方法/渐进安全" FOLDED="true" ID="ID_264897397" CREATED="1779762114799" MODIFIED="1779762254013">
+<node TEXT="引入安全系数n" ID="ID_948909245" CREATED="1779762120461" MODIFIED="1779762126533"/>
+<node TEXT="转化无法数学表达的为n的函数" ID="ID_1385133183" CREATED="1779762126683" MODIFIED="1779762135888"/>
+<node TEXT="有效计算" POSITION="bottom_or_right" ID="ID_1528807565" CREATED="1779762167875" MODIFIED="1779762169198">
+<node TEXT="PPT时间" ID="ID_1664925717" CREATED="1779762213968" MODIFIED="1779762225929"/>
+</node>
+<node TEXT="生成随机性" POSITION="bottom_or_right" ID="ID_1197095699" CREATED="1779762176398" MODIFIED="1779762177230"/>
+<node TEXT="可忽略的成功概率" POSITION="bottom_or_right" ID="ID_1672399524" CREATED="1779762179528" MODIFIED="1779762180422">
+<node TEXT="可忽略的特性2个" ID="ID_702853700" CREATED="1779762185690" MODIFIED="1779762191092">
+<node TEXT="negl1 + negl2 也是可忽略的" ID="ID_44458664" CREATED="1779762191499" MODIFIED="1779762199228"/>
+<node TEXT="p* negl1 也是可忽略的" ID="ID_1357852364" CREATED="1779762199548" MODIFIED="1779762209168"/>
+</node>
+</node>
+</node>
+<node TEXT="规约证明" FOLDED="true" ID="ID_650598191" CREATED="1779762261055" MODIFIED="1779762263812">
+<node TEXT="把计算破解密码方案的下界 转为数学难题的破解" ID="ID_1887902491" CREATED="1779762274316" MODIFIED="1779762292308"/>
+</node>
+<node TEXT="定义计算安全" FOLDED="true" ID="ID_1143205498" CREATED="1779762307465" MODIFIED="1779762314229">
+<node TEXT="窃听者不可区分实验的带有安全系数" ID="ID_1241460824" CREATED="1779762314232" MODIFIED="1779762330244"/>
+<node TEXT="等价：无论我们加密的是0 还是1 都是不可区分的" ID="ID_335883640" CREATED="1779762345627" MODIFIED="1779762348736"/>
+<node TEXT="等价：给出Enc 如果猜出m的第i个比特是可能的，那么区分m0和m1也是可以的" ID="ID_1147177927" CREATED="1779762359133" MODIFIED="1779762362915"/>
+<node TEXT="等价：如果敌手得到 密文能够简单的计算出f(m)，那么对于没有给出密文的敌手就可以按照同样的概率正确计算出f(m)" ID="ID_1372441449" CREATED="1779762372904" MODIFIED="1779762381394">
+<node TEXT="有效可采样" ID="ID_1411017825" CREATED="1779762381887" MODIFIED="1779762385366"/>
+</node>
+</node>
+<node TEXT="语义安全" ID="ID_1676103578" CREATED="1779762390207" MODIFIED="1779762391018">
+<node TEXT="与不可区分性是等价的" ID="ID_428561839" CREATED="1779762401045" MODIFIED="1779762415876"/>
+<node TEXT="外部情况" ID="ID_1475494200" CREATED="1779762417751" MODIFIED="1779762422221"/>
+</node>
+</node>
+</node>
+</map>
